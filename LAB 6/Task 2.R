c53d61ec2a4b25646bfc8716c4737601040222f1
harid8585 <harid8585@gmail.com>
2026-03-20 14:45:51 +0800

    Create Task 2.R

---
+++ LAB 6/Task 2.R
@@ -0,0 +1,10 @@
+#create vector
+name <- c("Anastasia", "Dima", "Michael", "Matthew", "Laura", "Kevin", "Jonas")
+score <- c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0)
+attempts <- c(1, 3, 2, 3, 2, 1, 2)
+qualify <- c("yes", "no", "yes", "no", "no", "no", "yes")
+
+#store data into data frame
+student_data <- data.frame(name,score,attempts,qualify)
+
+print(student_data)
